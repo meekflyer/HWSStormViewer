75f0653df0a1fa7592359e038545f551bad3a22d
Meek <meek@meekflyer.net>
2024-12-28 21:17:20 +0100

    Initial commit

---
+++ HWSStormViewer/HWSStormViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSStormViewerApp.swift
+//  HWSStormViewer
+//
+//  Created by Jonathan on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct HWSStormViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
